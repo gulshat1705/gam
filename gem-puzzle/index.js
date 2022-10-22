06b9a87cbaf0ec6a0a90e982b37dbb2c72a8f56f
gulshat <arkytbekovagulshat@gmail.com>
2022-10-23 00:02:10 +0600

    feat: add all layout by js

--- gem-puzzle/index.js
+++ gem-puzzle/index.js
@@ -14,9 +14,9 @@ const nav = document.createElement('nav');
 nav.className = 'nav';
 header.appendChild(nav);
 
-const menuArr = ['Shuffle and start', 'Stop', 'Save', 'Results'];
+const menuArr = ['shuffle and start', 'stop', 'save', 'results'];
 const ul = document.createElement('ul');
-// ul.className = 'menu';
+
 ul.setAttribute('class', 'menu');
 
 for(i=0; i<=menuArr.length - 1; i++) {
@@ -25,10 +25,12 @@ for(i=0; i<=menuArr.length - 1; i++) {
 
     li.setAttribute('class', 'menu-item');
     menuLink.setAttribute('class', 'menu-button');
-    
+        
     menuLink.innerHTML = menuArr[i];
-    console.log('inner', menuLink.innerHTML);
-    if(menuLink.innerHTML === 'Stop') {
+    
+    menuLink.setAttribute('id', ((menuLink.innerHTML).split(' '))[0]);
+
+    if(menuLink.innerHTML === 'stop') {
         menuLink.className += menuLink.className ? ' stop' : 'stop';
     }
     li.appendChild(menuLink);
@@ -39,23 +41,49 @@ nav.appendChild(ul);
 
 const main = document.createElement('main');
 main.className = 'main';
+header.after(main);
+const staticInfo = document.createElement('div');
+staticInfo.setAttribute('class', 'static-info');
+staticInfo.innerHTML = `
+                        <h5 class="static-info-text static-info__moves">Moves: <i class="moves">1</i></h5>
+                        <h5 class="static-info-text static-info__time">Time: <i class="times">10:55</i></h5>
+                    `
+main.appendChild(staticInfo);                    
 
-// header.appendBefore(main);
+const table = document.createElement('div');
+table.setAttribute('class', 'table');
+
+const sizesArr = [3, 4, 5, 6, 7, 8];
+let userSelect = sizesArr[1];
+let innerNum = (userSelect * userSelect) - 1;
+console.log(innerNum);
+
+const range = [...Array(innerNum - 1 + 1).keys()].map(x => x + 1);
+console.log(range.concat(''));
+const numsArr = range.concat('');
+
+const itemsShuffle = (arr) => {
+    arr.sort(() => Math.random() - 0.5);
+    console.log('shuffle');
+}
+
+const shuffle = document.querySelector('#shuffle');
+const stop = document.querySelector('#stop');
+const save = document.querySelector('#save');
+const results = document.querySelector('#results');
 
 
+shuffle.addEventListener('click', itemsShuffle(numsArr));
 
-// main.innerHTML += `<div class="static-info">
-// <h5 class="static-info-text static-info__moves">Moves: <i class="moves">1</i></h5>
-// <h5 class="static-info-text static-info__time">Time: <i class="times">10:55</i></h5>
-// </div>
-// <div class="table table-four">
-// <div class="table-items">5</div>
-// <div class="table-items empty-box"></div>
-// <div class="table-items">5</div>
-// <div class="table-items">5</div>
-// <div class="table-items">5</div>
-// <div class="table-items">5</div>                
-// </div>
+for (i=0; i<=range.length; i++) {
+    const tableItems = document.createElement('div');
+    tableItems.setAttribute('class', 'table-items');
+    tableItems.innerHTML = numsArr[i];
+    
+    table.appendChild(tableItems);
+}
+
+main.appendChild(table)
 // <div class="static-info">
 // <h5 class="static-info-text static-info__current-size">Frame size: <i class="current-size">4x4</i></h5>
 // </div>
